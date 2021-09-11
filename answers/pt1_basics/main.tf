a1946bf2af12b3676aeee4ae82fe74dcd441dde3
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-09-11 20:27:12 +0200

    basics - main.tf

--- answers/pt1_basics/main.tf
+++ answers/pt1_basics/main.tf
@@ -8,13 +8,20 @@ terraform {
   required_version = "1.0.5" // required_version setting applies only to the version of Terraform CLI. Terraform's resource types are implemented by provider plugins, whose release cycles are independent of Terraform CLI and of each other
 }
 
-provider "docker" { }
-
-resource "docker_image" "hello_word_image" {
-  name = "hello-world:latest"
+provider "docker" {
+  host = "npipe:////.//pipe//docker_engine" // windows: https://github.com/hashicorp/terraform-provider-docker/issues/180
 }
 
-resource "docker_container" "hello_word_container" {
-  image = docker_image.hello_word_image.id
-  name  = "hello-word-running"
+resource "docker_image" "http_server_image" {
+  name = "danjellz/http-server"
+}
+
+resource "docker_container" "http_server_container" {
+  image = docker_image.http_server_image.latest
+  name  = "http_server-running"
+
+  ports {
+    internal = "8080"
+    external = "8080"
+  }
 }
