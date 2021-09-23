9e84ebb21b1f4f6ab523b6d496c827289134f9ca
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-09-23 23:37:15 +0200

    pt4_foreach

--- answers/pt1_basics/pt4_foreach/main.tf
+++ answers/pt1_basics/pt4_foreach/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "2.15.0"
-    }
-  }
-  required_version = "1.0.5"
-}
-
-provider "docker" {
-  host = "npipe:////.//pipe//docker_engine"
-}
-
 resource "docker_image" "http_server_image" {
   name = "danjellz/http-server"
 }
