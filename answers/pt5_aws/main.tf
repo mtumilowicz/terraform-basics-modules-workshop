116211d9df8fc2e20e86cc4c7cd45ebc8187c202
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-09-25 13:46:17 +0200

    pt5_aws naming

--- answers/pt5_aws/main.tf
+++ answers/pt5_aws/main.tf
@@ -1,31 +1,16 @@
-provider "aws" {
-  region = "us-east-1"
-  access_key = "123"
-  secret_key = "xyz"
-  skip_credentials_validation = true
-  skip_requesting_account_id = true
-  skip_metadata_api_check = true
-  s3_force_path_style = true
-  endpoints {
-    ec2            = "http://localhost:4566"
-    s3             = "http://localhost:4566"
-  }
-}
-
-resource "aws_s3_bucket" "test-bucket" {
-  bucket = "demo-bucket-terraform"
-  acl    = "public-read"
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "test-bucket"
 
   tags = {
     Name = "S3Bucket"
   }
 }
 
-resource "aws_instance" "test-instance" {
+resource "aws_instance" "test_ec2" {
   ami           = "ami-0d57c0143330e1fa7"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "EC2Instance"
   }
 }
