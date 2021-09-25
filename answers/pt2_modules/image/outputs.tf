b012e96d05ab515b8544459e3a7100cdaedc7e8e
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-09-25 12:12:02 +0200

    pt2_modules naming

---
+++ answers/pt2_modules/image/outputs.tf
@@ -0,0 +1,3 @@
+output "image_name_output" {
+  value = docker_image.container_image_name.latest
+}
--- answers/pt2_modules/main.tf
+++ answers/pt2_modules/main.tf
@@ -9,7 +9,7 @@ module "container" {
   containers_to_create_count_input = each.value.containers_to_create_count
   for_each                         = local.containers
   container_prefix_name_input      = each.key
-  image_name_input                 = module.image[each.key].image_output
+  image_name_input                 = module.image[each.key].image_name_output
   internal_port_input              = each.value.int
   external_port_input              = each.value.ext
 }
