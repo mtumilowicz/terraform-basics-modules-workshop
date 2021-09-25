95e05d375fa92ecfc4d3b7cdd7c3ca68a7a99d93
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-09-25 18:15:57 +0200

    pt4_foreach outputs fix

--- answers/pt1_basics/pt4_foreach/outputs.tf
+++ answers/pt1_basics/pt4_foreach/outputs.tf
@@ -3,6 +3,6 @@ output "container_name_output" {
 }
 
 output "container_access_ip_port_output" {
-  value       = [for container in docker_container.http_server_container : join(":", [container.ip_address], container.ports[*]["external"])]
+  value       = [for container in docker_container.http_server_container : join(":", [container.network_data[0].ip_address], container.ports[*]["external"])]
   description = "IP + external port of the container"
 }
