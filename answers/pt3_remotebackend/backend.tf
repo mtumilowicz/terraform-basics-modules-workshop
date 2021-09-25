28039f18bb1b5c9a3a8af3b4f3d2abf9ff378c32
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-09-25 13:34:45 +0200

    pt3_remotebackend fix

--- answers/pt3_remotebackend/backend.tf
+++ answers/pt3_remotebackend/backend.tf
@@ -7,7 +7,7 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     force_path_style            = true
-    dynamodb_table              = "terraformlock"
+    dynamodb_table              = "terraform-lock"
     dynamodb_endpoint           = "http://localhost:4566"
     encrypt                     = true
   }
