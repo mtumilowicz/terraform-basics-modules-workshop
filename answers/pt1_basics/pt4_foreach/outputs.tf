6813a082e861c05668757e6116c411d949d1c3c0
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-09-25 18:37:28 +0200

    pt4_foreach simplification

--- answers/pt1_basics/pt4_foreach/outputs.tf
+++ answers/pt1_basics/pt4_foreach/outputs.tf
@@ -1,5 +1,5 @@
 output "container_name_output" {
-  value         = toset([for container in docker_container.http_server_container : container.name])
+  value = [for container in docker_container.http_server_container : container.name]
 }
 
 output "container_access_ip_port_output" {
