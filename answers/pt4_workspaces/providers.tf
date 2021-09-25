44b5e2fcb8473b8913bfe7cd201349d6a9fadcf1
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-09-25 13:33:21 +0200

    pt4_workspaces simplification

---
+++ answers/pt4_workspaces/providers.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = terraform.workspace
+  access_key = "123"
+  secret_key = "xyz"
+  skip_credentials_validation = true
+  skip_requesting_account_id = true
+  skip_metadata_api_check = true
+  s3_force_path_style = true
+  endpoints {
+    s3             = "http://localhost:4566"
+  }
+}
