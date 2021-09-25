ac93ed1c9a3cbedd0c730922045cb71c9566986b
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-09-25 17:23:28 +0200

    pt1_intro workshop hints

--- workshop/pt1_basics/pt1_intro/main.tf
+++ workshop/pt1_basics/pt1_intro/main.tf
@@ -1,13 +1,13 @@
 resource "docker_image" "http_server_image" {
-  name = "..."
+  name = "..." // "danjellz/http-server"
 }
 
 resource "docker_container" "http_server_container" {
-  image = "..."
-  name  = "..."
+  image = "..." // docker_image.http_server_image.latest
+  name  = "..." // "http_server-running"
 
   ports {
-    internal = 0
-    external = 0
+    internal = 0 // 8080
+    external = 0 // 8081
   }
 }
