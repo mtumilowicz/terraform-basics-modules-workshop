ce0824480dd181cdc5b4dbfa5fb490541605ae63
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-09-25 11:55:01 +0200

    pt2_modules fmt

--- answers/pt2_modules/locals.tf
+++ answers/pt2_modules/locals.tf
@@ -2,9 +2,9 @@ locals {
   containers = {
     http_server = {
       containers_to_create_count = length(var.external_ports[var.http_server])
-      image        = var.image[var.http_server]
-      int          = 8080
-      ext          = var.external_ports[var.http_server]
+      image                      = var.image[var.http_server]
+      int                        = 8080
+      ext                        = var.external_ports[var.http_server]
     }
   }
 }
--- answers/pt2_modules/main.tf
+++ answers/pt2_modules/main.tf
@@ -1,17 +1,17 @@
 module "image" {
-  source      = "./image"
-  for_each    = local.containers
+  source           = "./image"
+  for_each         = local.containers
   image_name_input = each.value.image
 }
 
 module "container" {
-  source              = "./container"
-  containers_to_create_count_input         = each.value.containers_to_create_count
-  for_each            = local.containers
-  container_name_input          = each.key
-  image_name_input         = module.image[each.key].image_output
-  internal_port_input = each.value.int
-  external_port_input = each.value.ext
+  source                           = "./container"
+  containers_to_create_count_input = each.value.containers_to_create_count
+  for_each                         = local.containers
+  container_name_input             = each.key
+  image_name_input                 = module.image[each.key].image_output
+  internal_port_input              = each.value.int
+  external_port_input              = each.value.ext
 }
 
 
